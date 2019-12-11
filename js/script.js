80762bf04c413e729620f6a0a0b85b256919ef43
Alexandro Santos <desenvolvedor.ti@msn.com>
2019-12-11 19:55:36 -0300

    Adicionados: cabecalho, secao noticias1, secao ultima hora

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+// Função para trocar o botão de pesquisa pelo edit de pesquisa:
+
+function btnPesquisarClick(elmentName) {
+    var btnPesquisar = document.getElementById('btnPesquisar');
+    var divCollapseExample = document.getElementById('divCollapseExample');
+
+    if (elmentName == 'btnPesquisar') {
+        btnPesquisar.style.display = 'none';
+    } else {
+        btnPesquisar.style.display = 'contents';
+        // divCollapseExample.display = 'none';
+    }
+}
